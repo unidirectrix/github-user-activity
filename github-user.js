314dc4fc432e0d375c76b73bc33d06688a7b0844
Unidirectrix <93390281+unidirectrix@users.noreply.github.com>
2026-01-22 20:37:37 +0800

    Add event update for issue and push events

--- github-user.js
+++ github-user.js
@@ -17,11 +17,12 @@ async function getGithubUser(username) {
 
         return await response.json();
     } catch (error) {
-        console.error('Fetch POST error:', error);
+        console.error('Fetch GET  error:', error);
     }
 }
 
 const body = await getGithubUser(uname);
+let outputString = "Output:\n";
 
 // Explicitly only watch for certain events
 const expectedEvents = [
@@ -33,4 +34,47 @@ const expectedEvents = [
     'WatchEvent',   // Star a repository
 ];
 
+let repos = [];
+let issueRepo;
+let issueAction;
 
+if (body.length !== 0){
+    for (let events of body) {
+        let recordedEvent = events["type"];
+        switch (recordedEvent) {
+            case 'CreateEvent':
+                break;
+            case 'DeleteEvent':
+                break;
+            case 'ForkEvent':
+                break;
+            case 'IssuesEvent':
+                issueAction = events["payload"]["action"];
+                issueRepo = events["repo"]["name"];
+                if (issueAction === 'opened'){
+                    outputString += `- Opened a new issue in ${issueRepo}\n`;
+                }else{
+                    outputString += `- ${issueAction.charAt(0).toUpperCase() + issueAction.slice(1)} an issue in ${issueRepo}\n`;
+                }
+                break;
+            case 'PushEvent':
+                issueRepo = events["repo"]["name"];
+                const index = repos.findIndex(rep => rep.repo === issueRepo);
+                if (index == -1){
+                    repos.push({"repo": issueRepo, "count": 1});
+                }else{
+                    repos[index]["count"] += 1;
+                }
+                break;
+            case 'WatchEvent':
+                break;
+        }
+    }
+    for (let repo of repos){
+        outputString += `- Pushed ${repo["count"]} commit/s to ${repo["repo"]}\n`;
+    }
+} else {
+    console.log("No recorded activity within the past 30 days.");
+}
+
+console.log(outputString);
